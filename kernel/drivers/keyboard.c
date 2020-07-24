2197aa79f069e42c533da877f8f67074ba1c78dd
Satyendra Kumar Banjare <satyendrabanjare99@gmail.com>
2020-07-24 17:02:33 +0530

    [kernel | Keyboard] added basic keys

--- kernel/drivers/keyboard.c
+++ kernel/drivers/keyboard.c
@@ -1,3 +1,9 @@
 #include "../include/drivers/keyboard.h"
 
+#define BACKSPACE			0x0E
+#define ENTER     			0x1C
+#define ARROW_UP			0x48
+#define	ARROW_DOWN			0x50
+#define	ARROW_LEFT			0x4B
+#define ARROW_RIGHT			0x4D
 
--- kernel/include/drivers/keyboard.h
+++ kernel/include/drivers/keyboard.h
@@ -1,6 +1,14 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#include <stdint.h>
+#include <stddef.h>
+
+#include "../cpu/isr.h"
+#include "../cpu/util.h"
+#include "../common/helper.h"
+#include "../terminal/terminal.h"
+
 #include "../codes.h"
 #include "../UserCredentials.h"
 
