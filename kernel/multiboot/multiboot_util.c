50b261b801791b6c0056c664d770d2e7ef7f9c8c
Satyendra Kumar Banjare <satyendrabanjare99@gmail.com>
2020-08-11 21:24:46 +0530

    [Multiboot] now prints working cmdline kernal name

--- kernel/multiboot/multiboot_util.c
+++ kernel/multiboot/multiboot_util.c
@@ -43,8 +43,8 @@ void print_module_info(struct multiboot_info * info) {
 			multiboot_module_t * module = (multiboot_module_t *) p_to_v(info->mods_addr);
 
 			print_log( (char *)(p_to_v(module->cmdline)) );   print_log("\n");
-			print_log("Mod Start (Virtual) : 0x"); print_log_int(module->mod_start,16);  print_log("\n");
-			print_log("Mod End (Virtual) : 0x"); print_log_int(module->mod_end,16);  print_log("\n");
+			print_log("Mod Start (Virtual) : 0x"); print_log_int(p_to_v(module->mod_start),16);  print_log("\n");
+			print_log("Mod End (Virtual) : 0x"); print_log_int(p_to_v(module->mod_end),16);  print_log("\n");
 
 			print_log("\n \n");
 		}
@@ -56,28 +56,29 @@ void print_multiboot_info(struct multiboot_info *mboot_ptr){
 
 	//print extra 0x for hex string
 
-	print_log("flags : 0x");			print_log_int(mboot_ptr->flags,16);				print_log("\n");
-	print_log("mem_lower (KB) : ");		print_log_int(mboot_ptr->mem_lower,10);			print_log("\n");
-	print_log("mem_upper (KB) : ");		print_log_int(mboot_ptr->mem_upper,10);			print_log("\n");
-	print_log("boot_device : 0x");		print_log_int(mboot_ptr->boot_device,16);		print_log("\n");
-	print_log("cmdline : 0x");			print_log_int(mboot_ptr->cmdline,16);			print_log("\n");
-	print_log("mods_count : ");			print_log_int(mboot_ptr->mods_count,10);		print_log("\n");
-	print_log("mods_addr : 0x");		print_log_int(mboot_ptr->mods_addr,16);			print_log("\n");
+	print_log("flags : 0x");			print_log_int(mboot_ptr->flags,16);					print_log("\n");
+	print_log("mem_lower (KB) : ");		print_log_int(mboot_ptr->mem_lower,10);				print_log("\n");
+	print_log("mem_upper (KB) : ");		print_log_int(mboot_ptr->mem_upper,10);				print_log("\n");
+	print_log("boot_device : 0x");		print_log_int(mboot_ptr->boot_device,16);			print_log("\n");
+	print_log("cmdline :");				print_log((char *)(p_to_v(mboot_ptr->cmdline)));	print_log("\n");
+	print_log("mods_count : ");			print_log_int(mboot_ptr->mods_count,10);			print_log("\n");
+	print_log("mods_addr : 0x");		print_log_int(p_to_v(mboot_ptr->mods_addr),16);				print_log("\n");
 
 	print_log("\n \n");
+	print_log("Flags Set :");
+	print_log("\n \n");
 
-	
 	if (mboot_ptr && (mboot_ptr->flags & MULTIBOOT_INFO_MEMORY) )
 	{
-		print_log("Got memory  ");print_log("\n");
+		print_log("MULTIBOOT_INFO_MEMORY  ");print_log("\n");
 	}
 	if (mboot_ptr && (mboot_ptr->flags & MULTIBOOT_INFO_BOOTDEV) )
 	{
-		print_log("Got bootdev  ");print_log("\n");
+		print_log("MULTIBOOT_INFO_BOOTDEV ");print_log("\n");
 	}
 	if (mboot_ptr && (mboot_ptr->flags & MULTIBOOT_INFO_CMDLINE) )
 	{
-		print_log("Got cmdline  ");print_log("\n");
+		print_log("MULTIBOOT_INFO_CMDLINE  ");print_log("\n");
 	}
 	if (mboot_ptr && (mboot_ptr->flags & MULTIBOOT_INFO_MODS) )
 	{
